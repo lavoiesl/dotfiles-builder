6b68185a218e53d87440e1ec284ae78cafea59f3
Sébastien Lavoie <github@lavoie.sl>
2014-10-29 01:09:55 -0400

    added some stuff from https://github.com/thoughtbot/dotfiles

--- dotfiles/.tmux.conf/parts/10_terminal.sh
+++ dotfiles/.tmux.conf/parts/10_terminal.sh
@@ -2,7 +2,7 @@
 
 # http://unix.stackexchange.com/questions/30058/how-can-i-check-which-terminal-definitions-are-available
 
-for term in xterm-256color xterm-color screen xterm rxvt; do
+for term in screen-256color xterm-256color xterm-color screen xterm rxvt; do
     if tput -t$term cols >/dev/null 2>/dev/null; then
         echo "set -g default-terminal \"$term\""
         break
