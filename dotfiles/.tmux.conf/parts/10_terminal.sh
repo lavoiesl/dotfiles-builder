7748e9177b3c0b04c621c2cf8e75b8510c0f8f1f
Sébastien Lavoie <github@lavoie.sl>
2014-10-31 14:59:00 -0400

    fixed terminal detection in tmux

--- dotfiles/.tmux.conf/parts/10_terminal.sh
+++ dotfiles/.tmux.conf/parts/10_terminal.sh
@@ -3,7 +3,7 @@
 # http://unix.stackexchange.com/questions/30058/how-can-i-check-which-terminal-definitions-are-available
 
 for term in screen-256color xterm-256color xterm-color screen xterm rxvt; do
-    if tput -t$term cols >/dev/null 2>/dev/null; then
+    if tput -T $term cols >/dev/null 2>/dev/null; then
         echo "set -g default-terminal \"$term\""
         break
     fi
