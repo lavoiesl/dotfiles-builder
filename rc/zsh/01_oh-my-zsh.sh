314d21ace7a6d54dbfa10c9af1407c105efaaf9f
Sébastien Lavoie <github@lavoie.sl>
2014-11-05 20:38:32 -0500

    added some oh-my-zsh plugins

--- rc/zsh/01_oh-my-zsh.sh
+++ rc/zsh/01_oh-my-zsh.sh
@@ -6,12 +6,16 @@ if [[ -d $HOME/.oh-my-zsh ]]; then
     echo 'COMPLETION_WAITING_DOTS="true"'
 
     echo 'plugins=('
+        echo battery
+        program_exists bower && echo bower
         program_exists brew && echo brew
+        program_exists brew-cask.rb && echo brew-cask
         program_exists composer && echo composer
         program_exists git && echo git
         program_exists php && echo symfony2
         program_exists ssh-agent && echo ssh-agent
         program_exists svn && echo svn
+        program_exists vagrant && echo vagrant
     echo ')'
 
     echo 'zstyle :omz:plugins:ssh-agent agent-forwarding on'
