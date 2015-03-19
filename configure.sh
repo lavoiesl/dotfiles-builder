4d9cb2d599ecd5839c9d44e461b41376071f3cde
Mathieu Lemoine <mathieu@wemakecustom.com>
2015-03-19 11:33:57 -0400

    Actually use DOTFILES_CONFIG_DIR for configuration instead of in-repo embeded config + Warning in case of empty/new config

--- configure.sh
+++ configure.sh
@@ -14,7 +14,12 @@ DOTFILES_INSTALL_PATH="${1:-${DOTFILES_INSTALL_PATH:-${HOME}}}"
 export DOTFILES_INSTALL_PATH="$(realpath "${DOTFILES_INSTALL_PATH}")"
 export DOTFILES_CONFIG_DIR="${DOTFILES_INSTALL_PATH}/.dotfiles-config"
 
-[ -d "${DOTFILES_CONFIG_DIR}" ] || mkdir "${DOTFILES_CONFIG_DIR}"
+if [ ! -d "${DOTFILES_CONFIG_DIR}" ]
+then
+    mkdir "${DOTFILES_CONFIG_DIR}"
+    echo "First time dotfiles-builder is run."
+    echo "You will need to import your configurations."
+fi
 
 for configure in "${DOTFILES_DIR}/configure"/*.sh; do
     . "${configure}"
