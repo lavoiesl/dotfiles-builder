885fb94467e578d3e9740f1c534a18fe3e1026c6
Sebastien Lavoie <lavoie_sl@apple.com>
2016-07-07 19:04:10 -0700

    Only attach tmux if session is not yet attached

--- rc/99_tmux.sh
+++ rc/99_tmux.sh
@@ -3,18 +3,20 @@
 if program_exists tmux; then
   echo 'if [ -z "$TMUX" ] && [ -t 1 -o  "$SSH_TTY" = "$(tty)" ]
 then
-  if ! tmux has-session -t main
-  then
+  if ! tmux has-session -t main; then
     tmux start-server
     tmux new-session -d -s main
   fi
-  tmux attach-session -t main
 
-  if [ -f ~/.no-screen ]
-  then
-    rm ~/.no-screen
-  else
-    exit
+  # Only attach if session is not yet attached
+  if tmux list-sessions -F "#{session_name} #{session_attached}" | grep -q "^main 0$"; then
+    tmux attach-session -t main
+
+    if [ -f ~/.no-screen ]; then
+      rm ~/.no-screen
+    else
+      exit
+    fi
   fi
 fi'
 fi
