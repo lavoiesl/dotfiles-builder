a01374a028c9705f9ad1c9da34b027862b4f9799
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2018-04-25 18:47:15 -0400

    warn when reattach is missing

--- dotfiles/.tmux.conf/parts/10_shell.sh
+++ dotfiles/.tmux.conf/parts/10_shell.sh
@@ -4,4 +4,8 @@ shell="${SHELL}"
 
 echo "set-option -g default-shell $shell"
 
-program_exists reattach-to-user-namespace && echo "set-option -g default-command \"reattach-to-user-namespace -l $shell\""
+if program_exists reattach-to-user-namespace; then
+    echo "set-option -g default-command \"reattach-to-user-namespace -l $shell\""
+else
+    echo "WARN: reattach-to-user-namespace is missing: brew install reattach-to-user-namespace"
+fi
