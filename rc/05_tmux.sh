0b7938eea626326e1ec6212aaf9cfc3b12636e08
Sebastien Lavoie <github@lavoie.sl>
2018-10-29 19:27:08 -0400

    Guard ZSH_TMUX_AUTOSTART

--- rc/05_tmux.sh
+++ rc/05_tmux.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 if program_exists tmux; then
-echo '
-export ZSH_TMUX_AUTOSTART=true
+    if [[ "$(uname)" == "Darwin" ]]; then
+        echo '
+if ! [[ "$(ps -ocomm= $PPID)" =~ \/Applications\/.+\.app ]]; then
+  export ZSH_TMUX_AUTOSTART=true
+fi'
+    else
+        echo '
+export ZSH_TMUX_AUTOSTART=true'
+    fi
+
+    echo '
 if [ -n "$TMUX" ]; then
     tmup ()
     {
