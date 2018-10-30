0d71860cc037c2200861ead1c8a228fb429abf91
Sebastien Lavoie <github@lavoie.sl>
2018-10-29 20:28:48 -0400

    fix PROMPT when not using oh-my-zsh

--- rc/zsh/30_prompt.sh
+++ rc/zsh/30_prompt.sh
@@ -5,8 +5,7 @@ if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
     if program_exists tput && tput colors >/dev/null; then
         echo "autoload -U promptinit"
         echo "promptinit"
-        # http://unix.stackexchange.com/questions/71974/preventing-zsh-from-using-aliases-in-cwd-prompt
-        echo "PROMPT='%F{magenta}[\${\${PWD/#%\$HOME/~}/#\$HOME\//~/}] %f'"
+        echo "PROMPT='%F{magenta}[%~] %f'"
     else
         echo "PROMPT='[%~] '"
     fi
