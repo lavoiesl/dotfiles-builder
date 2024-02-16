9c45eb7339545b0915341d55d291ad8bc34b4174
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2024-02-16 10:26:49 -0500

    Use sec gpg signing key

--- dotfiles/.gitconfig.dist/user/signingkey.sh
+++ dotfiles/.gitconfig.dist/user/signingkey.sh
@@ -2,8 +2,8 @@
 
 signingkey="$(git config --global user.signingkey)"
 
-[ -z "${signingkey}" ] && program_exists gpg && signingkey="$(gpg --list-secret-keys --with-colons | grep '^fpr' | grep -oE '[A-F0-9]+' | head -n1)"
+[ -z "${signingkey}" ] && program_exists gpg && signingkey="$(gpg --list-secret-keys --with-colons | grep '^sec' | grep -oE '[A-F0-9]{10,}' | head -n1)"
 
-echo "${signingkey}"
+echo "${signingkey}!"
 
 
